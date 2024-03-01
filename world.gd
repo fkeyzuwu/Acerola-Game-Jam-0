57e77531b08b2ee6415a16ef3b70a0a3c54187fa
fkeyz <fkeyzuwu@gmail.com>
2024-03-01 17:51:49 +0200

    get rid of warning

--- world.gd
+++ world.gd
@@ -3,5 +3,5 @@ extends Node3D
 func _ready() -> void:
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 	
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("quit"): get_tree().quit()
