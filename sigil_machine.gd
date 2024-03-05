eb71ca8d37affa9a39c609cd87ed0874ff19d49a
fkeyz <fkeyzuwu@gmail.com>
2024-03-05 21:07:00 +0200

    ok this works just no clamping yet

--- sigil_machine.gd
+++ sigil_machine.gd
@@ -14,48 +14,44 @@ var current_stone_sigil: SigilStone = null
 @onready var camera := get_viewport().get_camera_3d()
 
 func _ready() -> void:
-	deactivate()
+    deactivate()
 
 func _process(delta: float) -> void:
-	if current_stone_sigil:
-		var space_state = get_world_3d().direct_space_state
-		var mouse_pos = get_viewport().get_mouse_position()
-		var length = camera.global_position.distance_to(current_stone_sigil.global_position)
-		var from = camera.project_ray_origin(mouse_pos)
-		var mouse_pos_real = from + camera.project_ray_normal(mouse_pos) * length
-		mouse_pos_real.z = sigil_stones.global_position.z
-		prints("mouse_pos: ", mouse_pos_real)
-		prints("sigil stone: ", current_stone_sigil.global_position)
-		prints("center: ", sigil_stones.global_position)
-		var mouse_pos_real_real = Vector2(mouse_pos_real.x, mouse_pos_real.y)
-		var sigil_position = Vector2(sigil_stones.global_position.x, sigil_stones.global_position.y)
-		var angle_center_to_mouse = sigil_position.direction_to(mouse_pos_real_real).angle() - PI/2
-		prints("angle to mouse: ", rad_to_deg(angle_center_to_mouse))
-		
-		current_stone_sigil.rotation.y = angle_center_to_mouse
-		if Input.is_action_just_released("mouse_left"):
-			current_stone_sigil = null
+    if current_stone_sigil:
+        var space_state = get_world_3d().direct_space_state
+        var mouse_pos = get_viewport().get_mouse_position()
+        var length = camera.global_position.distance_to(current_stone_sigil.global_position)
+        var from = camera.project_ray_origin(mouse_pos)
+        var mouse_pos_real = from + camera.project_ray_normal(mouse_pos) * length
+        mouse_pos_real.z = sigil_stones.global_position.z
+        var mouse_pos_real_real = Vector2(mouse_pos_real.x, mouse_pos_real.y)
+        var sigil_position = Vector2(sigil_stones.global_position.x, sigil_stones.global_position.y)
+        var angle_center_to_mouse = sigil_position.direction_to(mouse_pos_real_real).angle() - PI / 2
+        prints("angle to mouse: ", rad_to_deg(angle_center_to_mouse))
+        current_stone_sigil.rotation.y = angle_center_to_mouse
+        if Input.is_action_just_released("mouse_left"):
+            current_stone_sigil = null
 
 func solved() -> void:
-	if light_tween: light_tween.kill()
-	light_tween = create_tween().set_trans(Tween.TRANS_SINE)
-	light_tween.tween_property(mesh_instance.mesh.material, "albedo_color", Color.LIGHT_GREEN, light_up_duration)
+    if light_tween: light_tween.kill()
+    light_tween = create_tween().set_trans(Tween.TRANS_SINE)
+    light_tween.tween_property(mesh_instance.mesh.material, "albedo_color", Color.LIGHT_GREEN, light_up_duration)
 
 func _on_player_detection_area_body_entered(_player: Player) -> void: #lightup
-	activate()
+    activate()
 
 func _on_player_detection_area_body_exited(_player: Player) -> void: #lightdown
-	deactivate()
+    deactivate()
 
 func activate() -> void:
-	if light_tween: light_tween.kill()
-	light_tween = create_tween().set_trans(Tween.TRANS_SINE)
-	light_tween.tween_property(mesh_instance.mesh.material, "albedo_color", Color.RED, light_up_duration)
-	sigil.reveal_sigil()
+    if light_tween: light_tween.kill()
+    light_tween = create_tween().set_trans(Tween.TRANS_SINE)
+    light_tween.tween_property(mesh_instance.mesh.material, "albedo_color", Color.RED, light_up_duration)
+    sigil.reveal_sigil()
 
 func deactivate() -> void:
-	if light_tween: light_tween.kill()
-	light_tween = create_tween().set_trans(Tween.TRANS_SINE)
-	light_tween.tween_property(mesh_instance.mesh.material, "albedo_color", Color.PURPLE, light_up_duration)
-	sigil.hide_sigil()
+    if light_tween: light_tween.kill()
+    light_tween = create_tween().set_trans(Tween.TRANS_SINE)
+    light_tween.tween_property(mesh_instance.mesh.material, "albedo_color", Color.PURPLE, light_up_duration)
+    sigil.hide_sigil()
 
