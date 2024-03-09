4bb333f8aa4b4f902a33f8e7160131caecbe5264
fkeyz <fkeyzuwu@gmail.com>
2024-03-09 16:17:36 +0200

    more updates to squid

--- squid_god.gd
+++ squid_god.gd
@@ -63,10 +63,10 @@ func _process(delta: float) -> void:
 			var prev_y = global_position.y
 			global_position = player.global_position - (player.orientation.basis.z * 45)
 			global_position.y = prev_y
-			look_at(player.global_position)
+			look_at_player()
 			eye.look_at_player(player)
 		SquidState.Messaging:
-			look_at(player.global_position)
+			look_at_player()
 			eye.look_at_player(player)
 		SquidState.ThorwingPlayer:
 			look_at_player()
